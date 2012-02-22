309a1c0d5cfd7fe695f667e1eead39c59f19647c
tom statter <github@autotelik.co.uk>
2012-02-22 22:22:32 +0000

    refactoring out big classes into small classes by concern. start spree 1.0 testing

--- sandbox/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Sandbox::Application.config.secret_token = '288a85a2353b7fe11c331b58a52701f97b58b9faaa6f54d1af4150c58285b90e03fc4aeebfd73b1d0cf0664b6f5af4f3aecb36c71760d934a3abd0e310c34c6e'
