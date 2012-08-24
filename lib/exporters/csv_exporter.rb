66cbc4c9e9630ca6a03bfbc4d5570d1ae2e0cb5d
Thomas Statter <tom@autotelik.co.uk>
2012-08-24 08:33:50 +0100

    start csv export task

--- lib/exporters/csv_exporter.rb
+++ lib/exporters/csv_exporter.rb
@@ -7,11 +7,13 @@
 #
 #
 require 'exporter_base'
+require 'csv'
 
 module DataShift
 
   class CsvExporter < ExporterBase
 
+    
     def initialize(filename)
       super(filename)
     end
@@ -22,7 +24,6 @@ module DataShift
     #
     def export(records, options = {})
       
-      require 'csv'
       File.open(filename, "w") do |csv|
         records.each do |r|
           next unless(r.is_a?(ActiveRecord::Base))
@@ -40,6 +41,59 @@ module DataShift
       end
     end
       
+    # Create an Excel file from list of ActiveRecord objects
+    # Specify which associations to export via :with or :exclude
+    # Possible values are : [:assignment, :belongs_to, :has_one, :has_many]
+    #
+    def export_with_associations(klass, items, options = {})
+        
+      MethodDictionary.find_operators( klass )
+         
+      MethodDictionary.build_method_details( klass )
+           
+      work_list = options[:with] || MethodDetail::supported_types_enum
+        
+      headers = []
+      puts "work_list :  [#{work_list.inspect}]"
+      
+      details_mgr = MethodDictionary.method_details_mgrs[klass]
+
+                        
+      data = []
+      
+      File.open(filename, "w") do |csv|
+ 
+   
+        # For each type belongs has_one, has_many etc find the operators
+        # and create headers, then for each record call those operators
+        work_list.each do |op_type|
+          
+          list_for_class_and_op = details_mgr.get_list(op_type)
+       
+          next if(list_for_class_and_op.nil? || list_for_class_and_op.empty?)
+
+          # method_details = MethodDictionary.send("#{mdtype}_for", klass)
+        
+          list_for_class_and_op.each do |md| 
+            headers << "#{md.operator}"
+            items.each do |i| 
+              data << i.send( md.operator )
+            end
+         
+          end
+
+          csv << headers.join(",")
+
+          csv << "\n"
+         
+          data.each do |d|
+            csv << d.join(",") << "\n"
+      
+          end
+        end
+      end
+    end
+    
     # Create CSV file representing supplied Model
     
     def generate(model, options = {})
