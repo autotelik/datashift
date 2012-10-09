c88dd93142f5d31ba1f2419c96612654a8d9e3d5
Thomas Statter <tom@autotelik.co.uk>
2012-10-09 17:46:17 +0100

    start paperclip attachment loader

--- spec/fixtures/models/owner.rb
+++ spec/fixtures/models/owner.rb
@@ -1,12 +1,7 @@
-require 'paperclip'
-
 class Owner < ActiveRecord::Base
+    
+  belongs_to :project, :dependent => :destroy
   
-  include Paperclip::Glue
+  has_many :digitals, :dependent => :destroy
   
-  belongs_to :project
-  
-  attr_accessible :avatar
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-
 end
---
+++ spec/paperclip_loader_spec.rb
@@ -0,0 +1,80 @@
+# Copyright:: (c) Autotelik Media Ltd 2011
+# Author ::   Tom Statter
+# Date ::     Aug 2011
+# License::   MIT
+#
+# Details::   Specs for base class Loader
+#
+require File.dirname(__FILE__) + '/spec_helper'
+
+require 'paperclip/attachment_loader'
+          
+describe 'PaperClip Bulk Loader' do
+
+  include_context "ActiveRecordTestModelsConnected"
+  
+  include_context "ClearAndPopulateProject"
+  
+  before(:each) do    
+    @attachment_klass = Digital
+    
+    @common_options = {:verbose => true }
+    
+    @attachment_path = File.join(fixtures_path, 'images')
+  end
+  
+  it "should create a new paperclip loader and define attachment class" do 
+    loader = DataShift::Paperclip::AttachmentLoader.new(@attachment_klass, nil, @common_options)
+    
+    loader.load_object_class.should == Digital
+    loader.load_object.should be_a Digital
+        
+    loader.attach_to_klass.should == NilClass  
+  end
+
+  it "should create loader,define attachment class and define class to attach to" do
+             
+    opts = { :attach_to_klass => Owner }.merge(@common_options)
+    
+    loader = DataShift::Paperclip::AttachmentLoader.new(@attachment_klass, nil, opts)
+    
+    loader.attach_to_klass.should == Owner
+  end
+  
+  it "should bulk load from a directory file system", :fail => true do
+   
+    # these names should be included in the attachment file name somewhere
+    ["DEMO_001", "DEMO_002", "DEMO_003", "DEMO_004"].each do |n|
+      Owner.create( :name => n )
+    end
+     
+    opts = {  :attach_to_klass => Owner, 
+              :attach_to_find_by_field => :name,
+              :split_file_name_on => '_'
+           }.merge(@common_options)
+    
+    loader = DataShift::Paperclip::AttachmentLoader.new(@attachment_klass, nil, opts)
+ 
+    loader.process_from_filesystem(@attachment_path, opts)
+  end
+  
+   it "should handle not beign able to fidn matching record" do
+   
+    # these names should be included in the attachment file name somewhere
+    names = ["DEMO_001", "DEMO_002", "DEMO_003", "DEMO_004"]
+    
+    names.each do |n|
+      Owner.create( :name => n )
+    end
+     
+    opts = { :attach_to_klass => Owner, :attach_to_find_by_field => :name }.merge(@common_options)
+    
+    
+    loader = DataShift::Paperclip::AttachmentLoader.new(@attachment_klass, nil, opts)
+ 
+    loader.process_from_filesystem(@attachment_path, opts)
+    
+    Dir.glob("MissingAttachmentRecords/*.jpeg", File::FNM_CASEFOLD).should have_exactly(names.size).items
+  end
+  
+end
