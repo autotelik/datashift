6422021764f5b2476b526aa8ba808ef561f9ae32
Thomas Statter <thomas.statter@mapscape.eu>
2017-09-22 17:25:33 +0200

    new install generator to create init block;

---
+++ lib/generators/datashift/install_generator.rb
@@ -0,0 +1,20 @@
+require 'rails/generators/base'
+
+module Datashift
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path("../../templates", __FILE__)
+
+      desc "Creates a DataShift initializer within your Rails application."
+      class_option :orm
+
+      def copy_initializer
+        template "datashift.rb", "config/initializers/datashift.rb"
+      end
+
+      def rails_4?
+        Rails::VERSION::MAJOR == 4
+      end
+    end
+  end
+end
